b3fff784288f3bc1fbe426c2589d779211b1f912
Ssousuke <wesleysousukee@gmail.com>
2024-02-20 22:53:57 -0300

    Adicionado conexão com o banco de dados SQLServer

--- src/RinhaBackend2024/Program.cs
+++ src/RinhaBackend2024/Program.cs
@@ -2,8 +2,6 @@ using Microsoft.EntityFrameworkCore;
 using RinhaBackend2024.Context;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddDbContext<AppDbContext>(x =>
-    x.UseSqlServer(builder.Configuration.GetConnectionString("Dev")));
 // Add services to the container.
 
 builder.Services.AddControllers();
@@ -11,18 +9,20 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+var dbHost = Environment.GetEnvironmentVariable("DB_HOST");
+var dbName = Environment.GetEnvironmentVariable("DB_NOME");
+var dbPassword = Environment.GetEnvironmentVariable("DB_SA_PASSWORD");
+
+var connectionString =
+    $"Data Source={dbHost}; Initial Catalog={dbName}; User id=sa; Password={dbPassword}; TrustServerCertificate=true";
+builder.Services.AddDbContext<AppDbContext>(options => options
+    .UseSqlServer(connectionString, x => x
+        .MigrationsAssembly(typeof(AppDbContext).Assembly.FullName)));
 
 var port = builder.Configuration["APIPORT"];
 builder.WebHost.UseUrls($"http://*:{port}");
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
 
 app.UseHttpsRedirection();
 
